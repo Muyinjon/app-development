2540131c34b449754176eac632105080a5a237cb
Muyinjon "M" Turobov <muyinjon2@gmail.com>
2024-07-14 19:21:46 -0400

    math update

--- ChildrenApp/script.js
+++ ChildrenApp/script.js
@@ -1,17 +1,50 @@
 let selectcolor = document.querySelectorAll(".selectcolor");
 let card = document.querySelector(".card");
 let colorselector = document.querySelector(".colorselector");
+let colorwindow = document.querySelector(".colorwindow");
 
-let color = ["red", "green", "blue", "yellow", "orange", "purple", "white", "black", "reset"];
-let bgcolortxt = document.querySelector(".selectcolor h1").textContent;
 
-let i = 0;
 
-colorselector.addEventListener("click", function () {
-    selectcolor[i].style.backgroundColor =  color[i];
-    bgcolortxt = document.querySelector(".selectcolor h1").textContent = color[i];
-    i++;
-    if (i == 8) {
-        i = 0;
-    }
-})
+for (let i = 0; i < selectcolor.length; i++) {
+    let bgcolortxt = document.querySelector(`.selectcolor:nth-child(${i + 1}) > h1`);
+
+    selectcolor[i].addEventListener("click", function () {
+        selectcolor[i].style.backgroundColor = bgcolortxt.innerHTML;
+        colorwindow.style.background = bgcolortxt.innerHTML;
+        colorwindow.innerHTML = bgcolortxt.innerHTML;
+        if (i === 3 || i === 6 || i === 8) {
+            selectcolor[i].style.color = 'black';
+        }
+    });
+}
+
+
+// calculator for html
+
+let calculatoncontainer = document.querySelector(".calculator-container");
+let operation = document.querySelectorAll(".operation button");
+let result = document.querySelector(".result");
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+
+for (let i = 0; i < operation.length; i++) {
+    operation[i].addEventListener("click", function () {
+        input1 = input1.value
+        input2 = input2.value
+        switch (operation[i].innerHTML) {
+            case "+":
+                result.innerHTML = Number(input1) + Number(input2);
+                break;
+            case "-":
+                result.innerHTML = Number(input1) - Number(input2);
+                break;
+            case "*":
+                result.innerHTML = Number(input1) * Number(input2);
+                break;
+            case "/":
+                result.innerHTML = Number(input1) / Number(input2);
+                break;
+        }
+    })
+}
+
