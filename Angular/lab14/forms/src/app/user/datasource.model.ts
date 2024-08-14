5a0f67e1b844cf9524aa3baaeb6b9d592083c7fe
Muyinjon "M" Turobov <muyinjon2@gmail.com>
2024-08-14 15:16:07 -0400

    lab14 done

--- Angular/lab14/forms/src/app/user/datasource.model.ts
+++ Angular/lab14/forms/src/app/user/datasource.model.ts
@@ -6,11 +6,12 @@ export class Datasource {
 
   //construct an array with different users
   constructor() {
-    this.users = new Array<User>();
-    new User(1, 'muyin', 'muyin@qq.com');
-    new User(2, 'muyin2', 'muyin2@qq.com');
-    new User(3, 'muyin3', 'muyin3@qq.com');
-    new User(4, 'muyin4', 'muyin4@qq.com');
+    this.users = new Array<User>(
+      new User(1, 'muyin', 'muyin@qq.com'),
+      new User(2, 'muyin2', 'muyin2@qq.com'),
+      new User(3, 'muyin3', 'muyin3@qq.com'),
+      new User(4, 'muyin4', 'muyin4@qq.com')
+    );
   }
   getuser(): User[] {
     return this.users;
--- Angular/lab14/forms/src/app/user/user.component.ts
+++ Angular/lab14/forms/src/app/user/user.component.ts
@@ -13,16 +13,44 @@ import { CommonModule } from '@angular/common';
 export class UserComponent {
   model: User_Repository = new User_Repository();
   newuser: User = new User();
+  parseJson: any;
+  parseJsonName: any;
+  parseJsonEmail: any;
+  usercounter: number = this.model.getuser().length;
 
   get jsonUser() {
     return JSON.stringify(this.newuser);
   }
 
   adduser(u: User) {
-    console.log(`New is ${u}`);
+    console.log(`New user is ` + this.jsonUser);
+    this.usercounter += 1;
+    this.model.adduser(
+      new User(this.usercounter, this.parseJsonName, this.parseJsonEmail)
+    );
   }
 
   displaylog(model: any) {
     console.log('New User :', model);
+  }
+
+  //form validation to set validation rules
+
+  formsubmit: boolean = false;
+
+  //submit form() method to handle the form data to submission
+
+  submitForm(form: any) {
+    this.formsubmit = true;
+    if (form.valid) {
+      this.parseJson = JSON.parse(JSON.stringify(this.newuser));
+      this.parseJsonName = this.parseJson.name;
+      this.parseJsonEmail = this.parseJson.email;
+      this.adduser(this.newuser);
+      this.adduser(this.newuser);
+      this.newuser = new User();
+      form.reset();
+      this.formsubmit = false;
+    }
   }
 }
